cd32960e3b234b08092ab1c2d53bdc45c4fe2df2
Ian Devins <ian.michael.devins@gmail.com>
2018-02-10 11:14:35 -0500

    Add working event listeners and DOM manipulation

--- script.js
+++ script.js
@@ -3,14 +3,66 @@ function computerPlay() {
 	return (choice == 0 ? 'Rock' : choice == 1 ? 'Paper' : 'Scissors')
 }
 
-function game(rounds) {
+//function game(rounds) {
 	var playerWins = 0;
 	var computerWins = 0;
+	
 
-	while (playerWins + computerWins < 5) {
+
+//	while (playerWins + computerWins < 5) {
 		
-		var computerSelection = computerPlay();
-		var playerSelection = prompt("Make a selection")
+		
+		const buttons = document.querySelectorAll('button');
+		
+		var playerSelection = '';
+		buttons.forEach((button) => {
+		 	button.addEventListener('click', (e) => {
+		 		var result = playRound(button.value, computerPlay());
+		 		displayRound(result);
+		 	})
+		 })
+		function displayRound(result){
+
+				const resultDiv = document.querySelector('#result');
+				var resultPara = document.createElement('p');
+				resultPara.classList.add('resultPara');
+				resultPara.textContent = result;
+				if (resultDiv.hasChildNodes()){
+					resultDiv.replaceChild(resultPara, resultDiv.childNodes[0]);
+				} else resultDiv.appendChild(resultPara);
+				const score = document.getElementById('score');
+				const pscore = document.getElementById('pscore');
+				const cscore = document.getElementById('cscore');
+				const winRow = document.getElementById('winRow');
+
+				pscore.textContent = playerWins;
+				cscore.textContent = computerWins;
+				var winner = "";
+				
+				if (playerWins + computerWins === 5){
+					playerWins > computerWins ? winner = "You win the game!" : winner = "Sad, you lost the game..."
+					winRow.textContent = winner
+					restartGame(score);
+				}
+		}
+
+		function restartGame(score) {
+			
+			buttons.forEach((button) => {
+		 		button.disabled = true;
+		 	});
+
+			const reloadbtn = document.createElement('button')
+			const para = document.createElement('p')
+
+			reloadbtn.textContent = "Restart?"
+			reloadbtn.addEventListener('click',(e) => location.reload())
+			
+			score.appendChild(para)
+			score.appendChild(reloadbtn);
+
+
+		}
 
 		function playRound(playerSelection, computerSelection) {
 		let player = playerSelection.toLowerCase();
@@ -29,9 +81,10 @@ function game(rounds) {
 			}
 		}
 
-		console.log(playRound(playerSelection, computerSelection))
-	}
 
-	return (playerWins > computerWins ? 'Player wins with ' +playerWins+ ' wins!' : 'Computer wins with ' +computerWins+ ' wins!')
- }
- console.log(game(5));
+	
+//	}
+
+//	return (playerWins > computerWins ? 'Player wins with ' +playerWins+ ' wins!' : 'Computer wins with ' +computerWins+ ' wins!')
+ //}
+ //console.log(game(5));
